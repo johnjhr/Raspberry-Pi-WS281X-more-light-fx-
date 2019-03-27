613b8f3b96f8bff5ef1929d1c598f82b274594b7
John Jaihar <37814942+johnjaiharjose@users.noreply.github.com>
2019-03-27 18:36:01 +0300

    Add files via upload

---
+++ reset.py
@@ -0,0 +1,18 @@
+#This is the main.  This meeds to import function_library.py in order to work.
+
+#Import the 'functions_library' which animates the LEDs in various ways.
+from function_library import *
+
+#Main program logic:
+if __name__ == '__main__':
+        #Process arguments
+        opt_parse()
+        #Create NeoPixel object with appropriate configuration.
+        strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+        #Intialize the library (must be called once before other functions).
+        strip.begin()
+
+        print ('Press Ctrl-C to quit.')
+        i=1
+        SetAll(strip, Color(0, 0, 0))
+        strip.show()
